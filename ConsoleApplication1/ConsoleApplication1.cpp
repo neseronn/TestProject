33f2cccd9a04cccd8b64a48015f42731a6cbc688
Liza <avdeenko.l26@mail.ru>
2021-07-14 13:29:56 +0300

    Добавлены структура и меню

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,11 +1,112 @@
-// ConsoleApplication1.cpp: определяет точку входа для консольного приложения.
-//
-
 #include "stdafx.h"
+#include <iostream>
+#include <conio.h>
+#include <Windows.h>
 
+#define SIZECHAR 50
 
-int _tmain(int argc, _TCHAR* argv[])
+enum menu {EXIT, ADD_STUDENT, PRINT_STUDENT,
+		   PRINT_ALL_STUDENT, WRITE_INTO_FILE,
+		   CLEAR_FILE};
+
+struct StudentBaseDate 
 {
-	return 0;
+    int id; 
+	char group[SIZECHAR];
+	char surname[SIZECHAR];
+    char name[SIZECHAR];
+	char CreditСard[SIZECHAR];
+	double stipend;
+	double SumStipend;
+	double MTotalCash;
+	double STotalCash;
+    StudentBaseDate* next; 
+};
+
+
+void _tmain()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int Menu();
+	int CmdMenu;
+	std::cout << "|===========================================|" 
+			  << std::endl;
+	std::cout << "| Программа по практике			    |" 
+			  << std::endl;
+	std::cout << "| Тема: \"Электронная ведомость.\"	    |"
+			  << std::endl;
+	std::cout << "| Вспомогательное приложение	            |" 
+			  << std::endl;
+	std::cout << "| для подсчёта стипендии студентов.         |" 
+			  << std::endl;
+	std::cout << "| Выполнили студенты группы ИВТ-101         |"
+			  << std::endl;
+	std::cout << "| Шитый А.Д."" Авдеенко Е.Е."
+				 " Кондратенко М.В. ""|"
+			  << std::endl;
+	std::cout<< "|===========================================|";
+
+	do
+	{
+		CmdMenu = Menu();
+		switch (CmdMenu)
+		{
+			case EXIT:
+				return;
+				break;
+			
+		}
+	}while(true);
+	system("pause");
 }
 
+int Menu()
+{
+	unsigned int NumberCmdMenu;
+	
+		for(;;)
+		{
+			std::cout << std::endl;
+			std::cout 
+			<< "|===========================================|" 
+			<< std::endl;
+			std::cout 
+			<< "|                   Меню                    |" 
+			<< std::endl;
+			std::cout 
+			<< "|0 - завершить работу                       |" 
+			<< std::endl;
+			std::cout 
+			<< "|1 - Добавить студента			    |" 
+			<< std::endl;
+			std::cout 
+			<< "|2 - Вывести информацию об студенте         |" 
+			<< std::endl;
+			std::cout 
+			<< "|3 - Вывести всех студентов                 |" 
+			<< std::endl;
+			std::cout 
+			<< "|4 - Cохранить, записав в файл              |" 
+			<< std::endl;
+			std::cout 
+			<< "|===========================================|" 
+			<< std::endl;
+			std::cout << "Ввод: ";
+			NumberCmdMenu = _getch() - '0';
+		if(NumberCmdMenu < EXIT ||
+		   NumberCmdMenu > WRITE_INTO_FILE)
+		{
+			std::cout<< "Ошибка - такой команды нет" 
+			<< std::endl;
+			continue;
+		}
+		else
+		{
+			std::cout <<"Номер команды меню: ";
+			std::cout << NumberCmdMenu <<std::endl;
+			return NumberCmdMenu;
+		}
+	return NumberCmdMenu;
+	}
+}
